aa0e581fd6cc91be83b25e3c33685de88f059662
Rolands Strakis <roland.strakis@gmail.com>
2016-04-22 16:58:59 +0300

    Done tutorial

--- EmailTutorial/Controllers/HomeController.cs
+++ EmailTutorial/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using EmailTutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -22,8 +26,35 @@ namespace EmailTutorial.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your application contact page.";
 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid) {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("rolands.strakis@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("roland.strakis@gmail.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient()) {
+                    // link:  http://www.mikesdotnetting.com/article/268/how-to-send-email-in-asp-net-mvc
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
             return View();
         }
     }
